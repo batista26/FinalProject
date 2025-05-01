eb48c2633ee349a0cdda829063f6979314add574
batista26 <118206108+batista26@users.noreply.github.com>
2025-04-30 22:29:18 -0400

    Add files via upload

---
+++ RGBLED.c/RGB.c
@@ -0,0 +1,8 @@
+#include "RGBLED.h"
+
+void RGB_Init(){
+    //Insert Intialization code here
+}
+void RGB_Set(char R, char G, char B){
+    //Insert code here 
+}
---
+++ RGBLED.c/main.c
@@ -0,0 +1,21 @@
+
+#include <msp430.h>
+#include "RGBLED.h" 
+#include "intrinsics.h"
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    RGB_Init();
+
+  
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+
+    while(1)
+    {
+        setRBGLED(0,0, 255);
+        _delay(100000)
+           
+    }
+}
