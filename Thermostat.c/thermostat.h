740dac51c6a2eb1c0cee36c2366ed8af00d8b10c
batista26 <118206108+batista26@users.noreply.github.com>
2025-05-10 23:04:17 -0400

    Update thermostat.h

---
+++ Thermostat.c/thermostat.h
@@ -0,0 +1,10 @@
+#ifndef THERMOSTAT_H
+#define THERMOSTAT_H
+
+// This function sets up the thermostat pins
+void thermostat_init(void);
+
+// This function checks the button and controls the LED
+void thermostat(void);
+
+#endif // THERMOSTAT_H
